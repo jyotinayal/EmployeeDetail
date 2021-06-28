5f88227a34a13e931001b310ad645b7f9b99afc1
jyoti nayal <jyoti.nayal@cuelogic.com>
2021-06-28 15:19:17 +0530

    comment removed, env added

--- models/db.js
+++ models/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const DB_NAME = process.env.DB_NAME;
+mongoose.connect(DB_NAME, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
--- service.js
+++ service.js
@@ -3,23 +3,16 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-// var bodyParser = require('body-parser')
 
 const employeeController = require('./controllers/employeeController');
 var app = express();
-
-// app.use(bodyParser.urlencoded({
-//     extended:true
-// }));
-// app.use(bodyParser.json());
-
 
 app.use(express.urlencoded({
     extended:true
 }));
 app.use(express.json());
 
-
+app.use('/employee',employeeController);     //add route for employee controller (middleware function)
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
@@ -28,4 +21,3 @@ app.listen(3000, () =>{
     console.log("Server started at port 3000")
 });
 
-app.use('/employee',employeeController);     //add route for employee controller (middleware function)
